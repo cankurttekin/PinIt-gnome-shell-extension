edc6a37c650346735144e8d664cfc798340350e1
cankurttekin <cankurttekin@gmail.com>
2024-06-12 16:38:15 +0300

    version 1 first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# WikiArt Wallpaper GNOME
+ GNOME Shell Extension to add your simle notes to the notification area.
+ 
+Click on tray icon, it will show up a dialog where you can input your notification title, message and optionally icon(default will be same as the extension icon).
+
+Tested on GNOME 42.
+
+_Important: Do not put information that you would care if it's lost._
+
+# Installing
+You can get this extension from [extensions.gnome.org](https://extensions.gnome.org/extension/) or you can download the files and copy pinit@cankurttekin to your `/.local/share/gnome-shell/extensions/` directory.
+
+# Contribute
+You can help with development by contributing code, testing on other GNOME versions and reporting issues.
+
+# Screenshots
+![Screenshot_1](/screenshots/tray.png)
+![Screenshot_2](/screenshots/dialog.png)
+![Screenshot_3](/screenshots/notifications.png)
+
---
+++ pinit@cankurttekin/extension.js
@@ -0,0 +1,193 @@
+const { St, Clutter, Gio, Shell } = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const MessageTray = imports.ui.messageTray;
+const ExtensionUtils = imports.misc.extensionUtils;
+const PopupMenu = imports.ui.popupMenu;
+
+const Me = ExtensionUtils.getCurrentExtension();
+
+class Dialog {
+    constructor() {
+        this._createDialog();
+    }
+
+    _createDialog() {
+        this.dialogOverlay = new St.Widget({
+            layout_manager: new Clutter.BinLayout(),
+            x_expand: true,
+            y_expand: true,
+            reactive: true,
+            can_focus: true,
+            visible: true,
+            opacity: 0,
+            style_class: 'dialog-overlay',
+        });
+
+        this.dialog = new St.BoxLayout({
+            vertical: true,
+            style_class: 'dialog',
+            x_expand: true,
+            y_expand: true,
+        });
+
+        this.titleEntry = new St.Entry({
+            style_class: 'dialog-entry',
+            can_focus: true,
+            hint_text: "Title",
+        });
+
+        this.messageEntry = new St.Entry({
+            style_class: 'dialog-entry',
+            can_focus: true,
+            hint_text: "Message",
+        });
+
+        this.iconDropdown = new St.Button({
+            label: "Choose icon",
+            style_class: 'dialog-dropdown',
+            can_focus: true,
+        });
+
+        this.iconMenu = new PopupMenu.PopupMenu(this.iconDropdown, 0.5, St.Side.TOP, 0);
+        Main.uiGroup.add_actor(this.iconMenu.actor);
+        this.iconMenu.actor.hide();
+
+        const iconNames = ['Pin', 'Calendar', 'Music', 'Information', 'Warning', 'Error'];
+        const iconMapping = {
+                    'Pin': 'view-pin-symbolic',
+                    'Calendar': 'office-calendar-symbolic',
+                    'Music': 'emblem-music-symbolic',
+                    'Information': 'dialog-information-symbolic',
+                    'Warning': 'dialog-warning-symbolic',
+                    'Error': 'dialog-error-symbolic'
+                };
+
+        //this.selectedIcon = iconNames[0];
+        this.selectedIcon = iconMapping['Pin'];
+        iconNames.forEach(iconName => {
+            let item = new PopupMenu.PopupMenuItem(iconName);
+            item.connect('activate', () => {
+                this.selectedIcon = iconMapping[iconName];
+                //log(this.selectedIcon);
+                //log(iconMapping[iconName]);
+                this.iconDropdown.label = iconName;
+            });
+            this.iconMenu.addMenuItem(item);
+        });
+
+        this.iconDropdown.connect('button-press-event', () => {
+            this.iconMenu.toggle();
+        });
+
+        this.submitButton = new St.Button({
+            label: "Submit",
+            style_class: 'dialog-button submit-button',
+        });
+
+        this.submitButton.connect('clicked', this._onSubmit.bind(this));
+
+        this.cancelButton = new St.Button({
+            label: "Cancel",
+            style_class: 'dialog-button cancel-button',
+        });
+
+        this.cancelButton.connect('clicked', this._destroyDialog.bind(this));
+
+        this.dialog.add_child(this.titleEntry);
+        this.dialog.add_child(this.messageEntry);
+        this.dialog.add_child(this.iconDropdown);
+        this.dialog.add_child(this.submitButton);
+        this.dialog.add_child(this.cancelButton);
+        this.dialogOverlay.add_child(this.dialog);
+
+        Main.layoutManager.addChrome(this.dialogOverlay);
+
+        this.dialogOverlay.ease({
+            opacity: 255,
+            duration: 250,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+        });
+
+        // Position the dialog at the top center
+        this.dialogOverlay.set_position(
+            Math.floor(Main.layoutManager.primaryMonitor.width / 2 - this.dialog.width / 2),
+            50
+        );
+    }
+
+    _onSubmit() {
+        let title = this.titleEntry.get_text();
+        let message = this.messageEntry.get_text();
+        let iconName = this.selectedIcon;
+        log(`Input title: ${title}, message: ${message}, icon: ${iconName}`);
+        this._showNotification(title, message, iconName);
+        this._destroyDialog();
+    }
+
+    _showNotification(title, message, iconName) {
+        let source = new MessageTray.Source(Me.metadata.name, iconName);
+        Main.messageTray.add(source);
+
+        let notificationIcon = new Gio.ThemedIcon({ name: iconName });
+
+        let notification = new MessageTray.Notification(source, title, message, { gicon: notificationIcon });
+        notification.setTransient(true);
+        source.showNotification(notification);
+    }
+
+    _destroyDialog() {
+        this.dialogOverlay.ease({
+            opacity: 0,
+            duration: 250,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            onComplete: () => {
+                this.dialogOverlay.destroy();
+            }
+        });
+    }
+}
+
+class MyExtension {
+    constructor() {
+        this._init();
+    }
+
+    _init() {
+        this._panelButton = new PanelMenu.Button(0.0, 'MyExtension', false);
+
+        let icon = new St.Icon({
+            gicon: new Gio.ThemedIcon({ name: 'view-pin-symbolic' }),
+            style_class: 'system-status-icon',
+        });
+
+        this._panelButton.add_child(icon);
+        this._panelButton.connect('button-press-event', this._showDialog.bind(this));
+        Main.panel.addToStatusArea('MyExtension', this._panelButton);
+    }
+
+    _showDialog() {
+        new Dialog();
+    }
+
+    enable() {
+        // Extension enable hook
+    }
+
+    disable() {
+        this._panelButton.destroy();
+    }
+}
+
+function init() {
+    return new MyExtension();
+}
+
+function enable() {
+    Me._extension = init();
+    Me._extension.enable();
+}
+
+function disable() {
+    Me._extension.disable();
+}
