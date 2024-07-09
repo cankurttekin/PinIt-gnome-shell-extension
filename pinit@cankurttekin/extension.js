d5370924915030713f3fab7cd38b7557b070a7f9
Can Kurttekin <cankurttekin@gmail.com>
2024-07-09 20:02:44 +0300

    issue #2 fixed
    Fix issues mentioned in #2 #3 for GNOME 45 and below versions
    
        Removed style.css and dependencies, now using only gnome system styles: custom themes won't break style changes with with this release.
        You can now navigate with tab button, you dont have to click each input field and submit button with mouse.
        I noticed some issues when closing dialog window. Removed mouse click event detection and pass it to your systems default. Now wherever you click, just like your quick settings, and notification panel, popupmenu will dissappear regardless.
        Input fields are long enough but if you want to add longer text now it can expand dynamically.

--- pinit@cankurttekin/extension.js
+++ pinit@cankurttekin/extension.js
@@ -1,275 +1,170 @@
-const { St, Clutter, Gio, Shell } = imports.gi;
+const { St, Clutter, Gio, GObject, Shell } = imports.gi;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const MessageTray = imports.ui.messageTray;
 const ExtensionUtils = imports.misc.extensionUtils;
 const PopupMenu = imports.ui.popupMenu;
-const Me = ExtensionUtils.getCurrentExtension();
-let PinIt;
 
-class Dialog {
-    constructor() {
-        if (!Dialog.instance) {
-            this._createDialog();
-            Dialog.instance = this;
-        }
-        return Dialog.instance;
-    }
+const PinExtension = ExtensionUtils.getCurrentExtension();
 
-    _createDialog() {
-        this.dialogOverlay = new St.Widget({
-            layout_manager: new Clutter.BinLayout(),
-            x_expand: true,
-            y_expand: true,
-            reactive: true,
-            can_focus: true,
-            visible: true,
-            opacity: 0,
-            style_class: 'dialog-overlay',
-        });
-
-        this.dialog = new St.BoxLayout({
-            vertical: true,
-            style_class: 'dialog',
-            x_expand: true,
-            y_expand: true,
-        });
-
-        this.titleEntry = new St.Entry({
-            style_class: 'dialog-entry',
-            can_focus: true,
-            hint_text: "Title",
-            track_hover: true,
-        });
-
-        this.messageEntry = new St.Entry({
-            style_class: 'dialog-entry',
-            can_focus: true,
-            hint_text: "Message",
-            track_hover: true,
-        });
-
-        this.iconBox = new St.BoxLayout({
-            vertical: false,
-            reactive: true,
-            track_hover: true,
-        });
-
-        this.iconMenu = new PopupMenu.PopupMenu(this.iconDropdown, 0.5, St.Side.TOP, 0);
-        Main.uiGroup.add_actor(this.iconMenu.actor);
-        this.iconMenu.actor.hide();
-
-        const iconNames = ['Pin', 'Calendar', 'Alarm', 'Music', 'Information', 'Warning', 'Error'];
-        const iconMapping = {
-            'Pin': 'view-pin-symbolic',
-            'Calendar': 'office-calendar-symbolic',
-            'Alarm': 'alarm-symbolic',
-            'Music': 'emblem-music-symbolic',
-            'Information': 'dialog-information-symbolic',
-            'Warning': 'dialog-warning-symbolic',
-            'Error': 'dialog-error-symbolic'
-        };
-
-        this.selectedIcon = iconMapping['Pin'];
-        
-        iconNames.forEach(iconName => {
-            let iconButton = new St.Button({
-                style_class: 'pinit-extension icon-button',
-                child: new St.Icon({
-                    gicon: Gio.icon_new_for_string(iconMapping[iconName]),
-                    style_class: 'icons',
-                }),
-            });
-
-            iconButton.connect('clicked', () => {
-                this.selectedIcon = iconMapping[iconName];
-                this._updateSelectedIcon(iconButton);
-            });
-            this.iconBox.add_child(iconButton);
-        });
-
-        this.submitButton = new St.Button({
-            label: "Submit",
-            style_class: 'dialog-button submit-button',
-        });
-
-        this.submitButton.connect('clicked', this._onSubmit.bind(this));
-
-        this.dialog.add_child(this.iconBox);
-        this.dialog.add_child(this.titleEntry);
-        this.dialog.add_child(this.messageEntry);
-        this.dialog.add_child(this.submitButton);
-        this.dialogOverlay.add_child(this.dialog);
-
-        Main.layoutManager.addChrome(this.dialogOverlay);
-
-        this.dialogOverlay.ease({
-            opacity: 255,
-            duration: 250,
-            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-        });
-
-        this._applyThemeStyles();
-        this._connectThemeChangeSignal();
-
-        // Position the dialog at the top center
-        this.dialogOverlay.set_position(
-            Math.floor(Main.layoutManager.primaryMonitor.width / 2 - this.dialog.width / 2),
-            50
-        );
-
-        this._outsideClickHandler = this._onOutsideClick.bind(this);
-        this._eventConnectionId = global.stage.connect('captured-event', this._outsideClickHandler);
-    }
-
-
-
-        
-    _applyThemeStyles() {
-        const settings = ExtensionUtils.getSettings();
-        const themeMode = settings.get_string('theme-mode');
-        const gtkSettings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
-        const gtkTheme = gtkSettings.get_string('gtk-theme');
-
-        if (themeMode === 'dark' || (themeMode === 'system' && gtkTheme.includes('dark'))) {
-            this.dialog.add_style_class_name('dialog-dark');
-            this.dialog.remove_style_class_name('dialog');
-            this.dialog.remove_style_class_name('dialog-oled');
-        } else if (themeMode === 'oled') {
-            this.dialog.add_style_class_name('dialog-oled');
-            this.dialog.remove_style_class_name('dialog-dark');
-            this.dialog.remove_style_class_name('dialog');
-        } else if (themeMode === 'light') {
-            this.dialog.add_style_class_name('dialog');
-            this.dialog.remove_style_class_name('dialog-dark');
-            this.dialog.remove_style_class_name('dialog-oled');
-        }  else {
-            if(gtkTheme.toLowerCase().includes('dark')) {
-            	this.dialog.add_style_class_name('dialog-dark');
-            	this.dialog.remove_style_class_name('dialog');
-            	this.dialog.remove_style_class_name('dialog-oled');
-            } else {
-            	this.dialog.add_style_class_name('dialog');
-            	this.dialog.remove_style_class_name('dialog-dark');
-            	this.dialog.remove_style_class_name('dialog-oled');
-            }
+const Pin = GObject.registerClass(
+    class Pin extends PanelMenu.Button {
+        _init(settings) {
+            super._init(0.0, _('test'));
             
+            this._icon = new St.Icon({
+                icon_name: 'view-pin-symbolic', // Change icon to a note symbol
+                style_class: 'system-status-icon',
+            });
+            this.add_child(this._icon);
+            
+            this.iconBox = new St.BoxLayout({
+                x_align: Clutter.ActorAlign.CENTER,
+                vertical: false,
+            });
+
+            const iconNames = ['Pin', 'Calendar', 'Music', 'Alarm', 'Information', 'Warning', 'Error'];
+            const iconMapping = {
+                'Pin': 'view-pin-symbolic',
+                'Calendar': 'x-office-calendar-symbolic',
+                'Music': 'emblem-music-symbolic',
+                'Alarm': 'alarm-symbolic',
+                'Information': 'dialog-information-symbolic',
+                'Warning': 'dialog-warning-symbolic',
+                'Error': 'dialog-error-symbolic'
+            };
+            this.selectedIcon = iconMapping['Pin'];
+            
+            // Create icon buttons and add them to the box layout
+            iconNames.forEach(iconName => {
+                let iconButton = new St.Button({
+                    style_class: 'quick-settings icon-button',
+                    style: 'margin: 4px;',
+                    child: new St.Icon({
+                        gicon: Gio.icon_new_for_string(iconMapping[iconName]),
+                        can_focus: true,
+                    }),
+                });
+                iconButton.connect('clicked', () => {
+                    this.selectedIcon = iconMapping[iconName];
+                });
+                this.iconBox.add_child(iconButton);
+            });
+            
+            this.titleEntry = new St.Entry({
+                hint_text: "Title",
+                can_focus: true,
+                track_hover: true,
+                style: 'margin-top: 8px;'
+            });
+
+            let vbox1 = new St.BoxLayout({
+                vertical: true,
+                style_class: 'popup-menu-box'
+            });
+            vbox1.add_child(this.titleEntry);
+
+            this.messageEntry = new St.Entry({
+                hint_text: "Message",
+                can_focus: true,
+                track_hover: true,
+                style: 'margin-top: 8px;',
+            });
+
+            let vbox2 = new St.BoxLayout({
+                vertical: true,
+                style_class: 'popup-menu-box'
+            });
+            vbox2.add_child(this.messageEntry);
+            
+            let submitIcon = new St.Icon({
+                gicon: Gio.icon_new_for_string('view-pin-symbolic'),
+                style_class: 'system-status-icon'
+            });
+            
+            // Create a label
+            let submitLabel = new St.Label({
+                text: ' Pin It',
+                style_class: 'system-menu-action'
+            });
+            
+            // Create a box layout to contain both icon and label
+            let submitBox = new St.BoxLayout({
+                style_class: 'icon-label-box',
+                vertical: false,
+                reactive: true,
+                track_hover: true,
+            });
+            submitBox.add_child(submitIcon);
+            submitBox.add_child(submitLabel);
+            
+            // Create the button with the icon and label
+            let submitButton = new St.Button({
+                style_class: 'icon-button', // Use GNOME Shell style class for button
+                child: submitBox, // Use the box layout containing icon and label as the child
+                x_align: Clutter.ActorAlign.END,
+                can_focus: true,
+                style: 'margin-top: 8px;'
+            });
+            
+            // Connect click event handler
+            submitButton.connect('clicked', () => {
+                let title = this.titleEntry.get_text();
+                let message = this.messageEntry.get_text();
+                let iconName = this.selectedIcon;
+                this._showNotification(title, message, iconName);
+                this.titleEntry.set_text('');
+                this.messageEntry.set_text('');
+                log('BUTON BASILDI');
+            });
+            
+
+            // Create a vertical box layout to arrange components vertically
+            let vbox = new St.BoxLayout({
+                vertical: true,
+                style_class: 'popup-menu-box'
+            });
+            vbox.add_child(this.iconBox);
+            vbox.add_child(vbox1);
+            vbox.add_child(vbox2);
+            vbox.add_child(submitButton);
+            let popupEdit = new PopupMenu.PopupMenuSection();
+            popupEdit.actor.add_child(vbox);
+            this.menu.addMenuItem(popupEdit);
+            this.menu.actor.add_style_class_name('note-entry');
+        }
+        
+        _showNotification(title, message, iconName) {
+            //let extensionObject = Extension.lookupByUUID('pinit@cankurttekin');
+            let source = new MessageTray.Source(PinExtension.metadata.name, iconName);
+            Main.messageTray.add(source);
+            let notificationIcon = new Gio.ThemedIcon({ name: iconName });
+            
+            let notification = new MessageTray.Notification(source, title, message, { gicon: notificationIcon });
+	    notification.setTransient(true);
+       	    source.showNotification(notification);
         }
     }
+);
 
-    _connectThemeChangeSignal() {
-        const gtkSettings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
-        gtkSettings.connect('changed::gtk-theme', () => {
-            this._applyThemeStyles();
-        });
-        const settings = ExtensionUtils.getSettings();
-        settings.connect('changed::theme-mode', () => {
-            this._applyThemeStyles();
-        });
-    }
-
-    _onOutsideClick(actor, event) {
-        if (event.type() === Clutter.EventType.BUTTON_PRESS) {
-            let [x, y] = event.get_coords();
-            if (!this.dialog.contains(global.stage.get_actor_at_pos(Clutter.PickMode.REACTIVE, x, y))) {
-                this._destroyDialog();
-            }
-        }
-    }
-
-    _onSubmit() {
-        let title = this.titleEntry.get_text();
-        let message = this.messageEntry.get_text();
-        let iconName = this.selectedIcon;
-        this._showNotification(title, message, iconName);
-        this._destroyDialog();
-    }
-
-    _updateSelectedIcon(selectedButton) {
-        this.iconBox.get_children().forEach(button => {
-            button.remove_style_class_name('selected-icon');
-        });
-        selectedButton.add_style_class_name('selected-icon');
-    }
-
-    _showNotification(title, message, iconName) {
-        let source = new MessageTray.Source(Me.metadata.name, iconName);
-        Main.messageTray.add(source);
-
-        let notificationIcon = new Gio.ThemedIcon({ name: iconName });
-
-        let notification = new MessageTray.Notification(source, title, message, { gicon: notificationIcon });
-        notification.setTransient(false);
-        notification.setUrgency('Urgency.LOW');
-
-        source.showNotification(notification);
-    }
-
-    _destroyDialog() {
-        if (this._eventConnectionId) {
-            global.stage.disconnect(this._eventConnectionId);
-            this._eventConnectionId = null;
-        }
-
-        this.dialogOverlay.ease({
-            opacity: 0,
-            duration: 250,
-            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-            onComplete: () => {
-                this.dialogOverlay.destroy();
-                Dialog.instance = null;
-            }
-        });
-    }
-}
-
-class MyExtension {
-    constructor() {
-        this._init();
-    }
-
-    _init() {
-        this._panelButton = new PanelMenu.Button(0.0, 'MyExtension', false);
-
-        let icon = new St.Icon({
-            gicon: new Gio.ThemedIcon({ name: 'view-pin-symbolic' }),
-            style_class: 'system-status-icon',
-        });
-
-        this._panelButton.add_child(icon);
-        this._panelButton.connect('button-press-event', this._showDialog.bind(this));
-        Main.panel.addToStatusArea('MyExtension', this._panelButton);
-    }
-
-    _showDialog() {
-        new Dialog();
-    }
-
-    enable() {
-        // Extension enable hook
-    }
-
-    disable() {
-        if (Dialog.instance) {
-            Dialog.instance._destroyDialog();
-        }
-        this._panelButton.destroy();
-    }
-}
 
 function init() {
     //return new MyExtension();
 }
 
 function enable() {
-    PinIt = new MyExtension();
-    //Me._extension = init();
-    //Me._extension.enable();
+    //PinIt = new MyExtension();
+    //this._settings = this.getSettings();
+    this._pin = new Pin(this._settings);
+    Main.panel.addToStatusArea(this.uuid, this._pin);
 }
 
 function disable() {
     //Me._extension.disable();
-    PinIt.disable();
-    PinIt = null;
+    //PinIt.disable();
+    //PinIt = null;
+    this._pin.destroy();
+    this._pin = null;
+    this._settings = null;
 }
 
