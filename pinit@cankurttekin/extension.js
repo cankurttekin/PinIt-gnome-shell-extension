9376c1ff3e015b99b5c177442a72c072bbe05983
can kurttekin <cankurttekin@gmail.com>
2025-01-21 03:30:18 +0300

    add timer to send notification

--- pinit@cankurttekin/extension.js
+++ pinit@cankurttekin/extension.js
@@ -20,6 +20,7 @@ import GObject from 'gi://GObject';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -49,7 +50,7 @@ const Pin = GObject.registerClass(
                 style_class: 'system-status-icon',
             });
             this.add_child(this._icon);
-            
+
             // Create extension functionality UI elements
             this.iconBox = this._createIconBox();
             this.titleEntry = this._createEntry(_("Title"));
@@ -66,16 +67,18 @@ const Pin = GObject.registerClass(
             vbox.add_child(this.titleEntry);
             vbox.add_child(this.messageEntry);
             vbox.add_child(submitButton);
-            
+
             const popupEdit = new PopupMenu.PopupMenuSection();
             popupEdit.actor.add_child(vbox);
             this.menu.addMenuItem(popupEdit);
             this.menu.actor.add_style_class_name('note-entry');
-            
+
             // Setting default notification icon
             this.selectedIcon = ICON_MAPPING['Pin'];
+
+            this.timerId = null;
         }
-        
+
         _createIconBox() {
             const box = new St.BoxLayout({
                 x_align: Clutter.ActorAlign.CENTER,
@@ -89,7 +92,7 @@ const Pin = GObject.registerClass(
             });
             return box;
         }
-        
+
         _createIconButton(iconName) {
             const iconButton = new St.Button({
                 style_class: 'quick-settings icon-button',
@@ -107,7 +110,7 @@ const Pin = GObject.registerClass(
             });
             return iconButton;
         }
-        
+
         // Create text field with hint
         _createEntry(hintText) {
             return new St.Entry({
@@ -117,9 +120,52 @@ const Pin = GObject.registerClass(
                 style: 'margin: 8px;'
             });
         }
-        
-        // Submit button with icon and label
+
+        // Submit button are with delay button
         _createSubmitButton() {
+            // Create cycling Delay button
+            // Delay values in minutes
+            const delayOptions = [0, 5, 15, 30, 60, 90, 120];
+            let currentDelayIndex = 0;
+
+            const delayIcon = new St.Icon({
+                gicon: Gio.icon_new_for_string('document-open-recent-symbolic'),
+                style_class: 'system-status-icon'
+            });
+
+            const delayLabel = new St.Label({
+                text: ` ${delayOptions[currentDelayIndex]}`,
+                style_class: 'system-menu-action'
+            });
+
+            const delayBox = new St.BoxLayout({
+                style_class: 'icon-label-box',
+                vertical: false,
+                reactive: true,
+                track_hover: true,
+                x_align: Clutter.ActorAlign.CENTER
+            });
+
+            delayBox.add_child(delayIcon);
+            delayBox.add_child(delayLabel);
+
+            const delayButton = new St.Button({
+                //label: `${delayOptions[currentDelayIndex]}`,
+                child: delayBox,
+                style_class: 'quick-settings icon-button',
+                style: 'margin: 4px; margin-top: 8px;',
+                can_focus: true,
+                x_expand: true,
+                x_align: Clutter.ActorAlign.START,
+            });
+
+            delayButton.connect('clicked', () => {
+                // Cycle through delay options
+                currentDelayIndex = (currentDelayIndex + 1) % delayOptions.length;
+                delayLabel.text = ` ${delayOptions[currentDelayIndex]}`;
+            });
+
+            // Create "Pin It" button
             const submitIcon = new St.Icon({
                 gicon: Gio.icon_new_for_string(ICON_MAPPING['Pin']),
                 style_class: 'system-status-icon'
@@ -135,6 +181,7 @@ const Pin = GObject.registerClass(
                 vertical: false,
                 reactive: true,
                 track_hover: true,
+
             });
             submitBox.add_child(submitIcon);
             submitBox.add_child(submitLabel);
@@ -142,24 +189,43 @@ const Pin = GObject.registerClass(
             const submitButton = new St.Button({
                 style_class: 'icon-button',
                 child: submitBox,
-                //x_align: Clutter.ActorAlign.END,
                 can_focus: true,
                 style: 'margin: 8px;',
+                x_align: Clutter.ActorAlign.END,
             });
-            // submitButton.add_style_class_name('button-accent'); 
 
-            // Click handler for the submit button
+            // Handle notification submission with delay
             submitButton.connect('clicked', () => {
                 const title = this.titleEntry.get_text();
                 const message = this.messageEntry.get_text();
                 const iconName = this.selectedIcon;
-                this._showNotification(title, message, iconName);
+                const delay = delayOptions[currentDelayIndex] * 60; // Convert minutes to seconds
+
+                if (delay === 0) {
+                    this._showNotification(title, message, iconName);
+                } else {
+                    this._scheduleNotification(title, message, iconName, delay);
+                }
+
                 this.titleEntry.set_text('');
                 this.messageEntry.set_text('');
             });
-            return submitButton;
+
+            // Combine delay button and submit button
+            const buttonBox = new St.BoxLayout({ vertical: false, x_expand: true });
+            buttonBox.add_child(delayButton);
+            buttonBox.add_child(submitButton);
+
+            return buttonBox;
         }
-        
+
+        _scheduleNotification(title, message, iconName, delay) {
+            this.timerId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, delay, () => {
+                this._showNotification(title, message, iconName);
+                return false; // timer runs only once
+            });
+        }
+
         _updateActiveButton(button) {
             if (this.activeButton) {
                 this.activeButton.remove_style_class_name('button-accent');
@@ -167,11 +233,11 @@ const Pin = GObject.registerClass(
             button.add_style_class_name('button-accent');
             this.activeButton = button;
         }
-        
+
         // Creating notification with title, message and icon
         _showNotification(title, message, iconName) {
             const notificationIcon = new Gio.ThemedIcon({ name: iconName });
-            const source = new MessageTray.Source({
+            let source = new MessageTray.Source({
                 title: _('PinIt'),
                 iconName: 'view-pin-symbolic',
             });
@@ -193,6 +259,14 @@ const Pin = GObject.registerClass(
             Main.messageTray.add(source);
             source.addNotification(notification);
         }
+
+        // Cancel the timer when the extension is disabled
+        _cancelTimer() {
+            if (this.timerId !== null) {
+                GLib.source_remove(this.timerId);
+                this.timerId = null;
+            }
+        }
     }
 );
 
@@ -203,6 +277,7 @@ export default class PinItExtension extends Extension {
         Main.panel.addToStatusArea(this.uuid, this._pin);
     }
     disable() {
+        this._pin._cancelTimer();
         this._pin.destroy();
         this._pin = null;
         this._settings = null;
